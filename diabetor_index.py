b378f9206198f2617ede42101d08c36f61a97a27
PrayogaRam <prayogaproject22@gmail.com>
2024-09-05 21:15:01 +0700

    diabetor_index.py

--- diabetor_index.py
+++ diabetor_index.py
@@ -62,11 +62,11 @@ def main():
     # Menampilkan prediksi setelah tombol diklik
     if st.button("Deteksi Diabetes"):
         prediction = float(model.predict(input_data))
-        if prediction < 2 or prediction >= 1:
+        if prediction == 0.5:
             st.warning("⚠️ Anda mungkin sedang dalam tahap pre-diabetes. Harap perhatikan gaya hidup dan konsultasikan dengan dokter.")
-        elif prediction >= 2:
+        elif prediction == 1:
             st.warning("⚠️ Anda mungkin memiliki diabetes tipe 2. Harap perhatikan gaya hidup dan konsultasikan dengan dokter.")
-        elif prediction < 1 or prediction >= 0:
+        elif prediction >= 0:
             st.success("✅ Anda kemungkinan besar tidak memiliki diabetes. Tetap jaga kesehatan Anda!")
             st.balloons()
 
